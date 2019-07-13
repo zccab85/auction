f7f5469f634ab919262906662000fae922d1f702
zccab85 <zccab85@gmail.com>
2019-07-13 17:40:23 +0900

    delete function complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,12 @@ class ItemsController < ApplicationController
     redirect_to @item
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
   private
 
   def item_params
