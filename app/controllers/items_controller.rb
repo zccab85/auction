ff291a50eda7b0f976046349c8d4d2f01799f2da
zccab85 <zccab85@gmail.com>
2019-07-13 17:18:23 +0900

    edit function complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,18 @@ class ItemsController < ApplicationController
   redirect_to @item
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
+
   private
+
   def item_params
   params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
   end
