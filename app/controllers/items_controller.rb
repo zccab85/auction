f3b4519e938bd89779d34e1aa732faf6b5f63921
zccab85 <zccab85@gmail.com>
2019-07-13 16:48:19 +0900

    item registration function complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,19 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new 
+  end
+
+  def create
+  @item = Item.new(item_params)
+  @item.save
+  redirect_to @item
+  end
+
+  private
+  def item_params
+  params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+  end
 end
